3760274f55e7e6edcbf7787383bd86c38c026323
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2013-12-12 10:29:57 -0500

    Set up for Tide SDK

--- Life Saving/Resources/JS/init.js
+++ Life Saving/Resources/JS/init.js
@@ -7,15 +7,11 @@ ajSettings.url = "http://server.local:8080/lifeSaving/LAKE/Superior";
 var ajResp;
 var timer;
 
-//Animation Framerate
-$.fx.interval = 13;
-
 /* Runs after page loaded */
 $(document).ready(init);
 
 function init(){
 	
-	air.Mouse.hide();
-	alert("init");
+
 
 }
