959d30d54d34b3d5b5bb24b5c035b40025ddeee1
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2014-01-15 10:26:16 -0500

    New Scroller, Resized Pics

--- Life Saving/Resources/init.js
+++ Life Saving/Resources/init.js
@@ -5,3 +5,5 @@ ajSettings.url = "http://monadnock.or.gs:8080/lifeSaving/LAKE/Superior";
 
 var ajResp;
 var timer;
+
+$.fx.interval = 1;
--- Life Saving/Resources/selectors.js
+++ Life Saving/Resources/selectors.js
@@ -1,6 +1,6 @@
 
 var adaEnabled = false;
-var currentScreen = 0;
+var currentScreen = 1;
 var selectedOffset;
 var selectedCenter;
 var selectedHeight;
@@ -14,12 +14,39 @@ var idleTimer;
 
 $(document.body).click(function(e) {
 	userStarted();
-	
 })
 
 function restart() {
 	document.location.href='';
 }
+
+
+attractLoop();
+function attractLoop(){
+		
+	$("#attractBook").animate({left: "-=1280px"}, 15000, "linear", function() {
+		$("#attractBook").css({left: 1280});
+		
+		$("#attractBook").animate({left: "-=1280px"}, 15000, "linear", function() {
+			attractLoop();
+		});
+		
+	});
+	
+	$("#attractFilm").animate({left: "-=2560px"}, 30000, "linear", function() {
+		$("#attractFilm").css({left: 1280});
+	});
+}
+
+$("#attractBook").click(function(e) {
+	$("#attractBook").animate({opacity:0}, 250, function() {});
+	$("#attractFilm").animate({opacity:0}, 250, function() {});
+	$("#attractLoopBG").animate({opacity:0}, 250, function() {
+		$("#attractLoopBG").css({left: 8000});
+	});
+	currentScreen = 1;
+});
+
 
 function promptIdleUser() {   
     clearInterval(idleTimer);
@@ -46,13 +73,12 @@ function promptIdleUser() {
 	}); // Shows the idle alert box.
 }
 
+
 function userStarted() {
 	clearInterval(idleTimer);
 	console.log("Clear Interval");
 	idleTimer = setInterval(promptIdleUser, 120000); // 30000
 }
-
-
 
 
 function checkPosition() { //gets called every update when slider is moving
@@ -62,7 +88,7 @@ function checkPosition() { //gets called every update when slider is moving
 	selectedHeight = $(".selected").innerHeight();
 	selectedCenter = (selectedHeight/2);
 	selectedMiddle = (selectedOffset.top + selectedCenter);
-	//air.Introspector.Console.log(selectedMiddle);
+
 	if (selectedMiddle < 185) {
 		$(".selected").animate({opacity:0.01}, 150, function() {});
 		$(".selected + .summary").addClass("selected");
@@ -77,60 +103,62 @@ function checkPosition() { //gets called every update when slider is moving
 }
 
 
-function clearMap() {
-	$(".nextBttn").removeClass("Superior Michigan Huron Erie Ontario");
-}
-
-
 /*-- - --*/
 
 
 $(".mapBttn").click(function(e) {
-	if (currentScreen == 0) {
-		$(".mapTitle").fadeIn(400).animate({left: "+=1280", opacity:1}, 500, function() {});
+	if (currentScreen == 1) {
+
 		$(".mapBttn").fadeOut(500);
-		$(".nextBttn").animate({opacity:1}, 500, function() {});
 		$(".backBttn").animate({opacity:1}, 500, function() {});
 		$(".excerptTitle").empty();
+		$(".excerptTitle").animate({opacity:1}, 250, function() {});
 		
-		currentScreen = 1;
-		clearMap();
+		//Go to Excerpt Screens
+		$(".mapTitle").fadeIn( 250 ).animate({left: "-=1280", opacity:1}, 250, function() {
+			$(".dateContainer").animate({left: "+=460", opacity:1}, 250, function() {});
+			$(".gradientBG").animate({left: "+=460", opacity:1}, 250, function() {});
+			$(".summaryHeader").animate({left: "+=460", opacity:1}, 250, function() {});
+			$(".dates").animate({opacity:1}, 250, function() {});
+			$(".reports").animate({opacity:1}, 250, function() {});
+			
+			
+		});
+		currentScreen = 3;
+		$(this).toggleClass("disableClick");
+		$("body").toggleClass("wholeMap");
+		
 		
 			if ($(this).hasClass("Superior")) {
 				ajSettings.url = "http://monadnock.or.gs:8080/lifeSaving/LAKE/Superior";
 				header = "<h2><u>Excerpt from Annual Report of the U.S. Life-Saving Service<u/></h2>";
 				$( ".mapTitle" ).empty();
 				$( ".mapTitle" ).append( "<h1>Lake Superior</h1>" );
-				$(".nextBttn").addClass("Superior");
-				$(".excerptTitle").append("The <i>Waldo</i>");
+				$(".excerptTitle").append("The <i>Waldo</i>, 1913");
 			} else if ($(this).hasClass("Michigan")) {
 				ajSettings.url = "http://monadnock.or.gs:8080/lifeSaving/LAKE/Michigan";
 				header = "<h2><u>Excerpt from Annual Report of the U.S. Life-Saving Service<u/></h2>";
 				$( ".mapTitle" ).empty();
 				$( ".mapTitle" ).append( "<h1>Lake Michigan</h1>" );
-				$(".nextBttn").addClass("Michigan");
-				$(".excerptTitle").append("The <i>Granada</i>");
+				$(".excerptTitle").append("The <i>Granada</i>, 1880");
 			} else if ($(this).hasClass("Huron")) {
 				ajSettings.url = "http://monadnock.or.gs:8080/lifeSaving/LAKE/Huron";
 				header = "<h2><u>Excerpt from the Report of District Superintendent Joseph Sawyer<u/></h2>";
 				$( ".mapTitle" ).empty();
 				$( ".mapTitle" ).append( "<h1>Lake Huron</h1>" );
-				$(".nextBttn").addClass("Huron");
-				$(".excerptTitle").append("The <i>J.H. Magruder</i>");
+				$(".excerptTitle").append("The <i>J.H. Magruder</i>, 1880");
 			} else if ($(this).hasClass("Erie")) {
 				ajSettings.url = "http://monadnock.or.gs:8080/lifeSaving/LAKE/Erie";
 				header = "<h2><u>Excerpt from Annual Report of the U.S. Life-Saving Service<u/></h2>";
 				$( ".mapTitle" ).empty();
 				$( ".mapTitle" ).append( "<h1>Lake Erie</h1>" );
-				$(".nextBttn").addClass("Erie");
-				$(".excerptTitle").append("The <i>Sophia Minch</i>");
+				$(".excerptTitle").append("The <i>Sophia Minch</i>, 1884");
 			} else if ($(this).hasClass("Ontario")) {
 				ajSettings.url = "http://monadnock.or.gs:8080/lifeSaving/LAKE/Ontario";
-				header = "<h2><u>Excerpt from letter by the Secretary of the Treasury to Keeper Gray and crew<u/></h2>";
+				header = "<h2><u>Excerpt from the letter commending Keeper Gray and crew<u/></h2>";
 				$( ".mapTitle" ).empty();
 				$( ".mapTitle" ).append( "<h1>Lake Ontario</h1>");
-				$(".nextBttn").addClass("Ontario");
-				$(".excerptTitle").append("The <i>John R. Noyes</i>");
+				$(".excerptTitle").append("The <i>John R. Noyes</i>, 1902");
 			}
 		
 		ajResp = $.get(ajSettings.url).success(function() {
@@ -147,7 +175,6 @@ $(".mapBttn").click(function(e) {
 			
 			loadExcerpt();
 		});
-		
 	}
 });
 
@@ -165,16 +192,16 @@ function loadExcerpt () {
 
 
 $(".backBttn").click(function(e) {
-	if (currentScreen == 1) {
+	if (currentScreen == 2) {
 		$(".mapTitle").fadeIn( 400 ).animate({left: "-=1280", opacity:1}, 500, function() {});
 		$(".mapBttn").fadeIn(500);
-		$(".nextBttn").animate({opacity:0}, 500, function() {});
 		$(".reports").animate({opacity:0}, 0, function() {});
 		$(".backBttn").animate({opacity:0}, 500, function() {});
-		currentScreen = 0;
-	} else if (currentScreen == 2) {
+		currentScreen = 1;
+	} else if (currentScreen == 3) {
 		$(".dateContainer").animate({left: "-=460", opacity:0}, 0, function() {});
 		$(".gradientBG").animate({left: "-=460", opacity:0}, 0, function() {});
+		$(".summaryHeader").animate({left: "-=460", opacity:0}, 0, function() {});
 		$(".dates").animate({opacity:0}, 100, function() {});
 		$(".excerptTitle").animate({opacity:0}, 0, function() {});
 		$(".reports").animate({opacity:0}, 0, function() {});
@@ -182,29 +209,12 @@ $(".backBttn").click(function(e) {
 		$(".mapBttn").fadeIn(0);
 		$( ".dates" ).empty();
 		$("body").toggleClass("wholeMap");
-		currentScreen = 0;
-		$(".nextBttn").toggleClass("disableClick");
+		currentScreen = 1;
+		$(".mapBttn").fadeIn(500);
+		$(".mapBttn").removeClass("disableClick");
 	}
 });
 
-
-$(".nextBttn").click(function(e) {
-	if (currentScreen == 1) {
-		//Go to Excerpt Screens
-		$(".mapTitle").fadeIn( 400 ).animate({left: "-=1280", opacity:1}, 500, function() {
-			$(".dateContainer").animate({left: "+=460", opacity:1}, 500, function() {});
-			$(".gradientBG").animate({left: "+=460", opacity:1}, 500, function() {});
-			$(".dates").animate({opacity:1}, 500, function() {});
-			$(".reports").animate({opacity:1}, 500, function() {});
-			
-			$(".excerptTitle").animate({opacity:1}, 500, function() {});
-		});
-		currentScreen = 2;
-		$(".nextBttn").animate({opacity:0}, 500, function() {});
-		$(this).toggleClass("disableClick");
-		$("body").toggleClass("wholeMap");
-	}
-});
 
 
 $( ".scroll-pane" ).on( "slidestop", function( event, ui ) {
