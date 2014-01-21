06d04ece3b78c07025dd83cd9c014f011592fe8a
theraydog <ray.ortgiesen@gmail.com>
2014-01-21 13:56:25 -0500

    Added new swf video

--- Life Saving/Resources/init.js
+++ Life Saving/Resources/init.js
@@ -10,7 +10,7 @@ var clickSnd = Ti.Media.createSound('SOUND/click.wav');
 
 var videoHTML;
 
-videoHTML = "<object id='myId' classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='640' height='480'><param name='movie' value='LifeSaving_converted.swf' /><!--[if !IE]>--><object type='application/x-shockwave-flash' data='LifeSaving_converted.swf' width='640' height='480'><!--<![endif]--><p>Flash Not Installed</p><!--[if !IE]>--></object><!--<![endif]--></object>";
+videoHTML = "<object id='myId' classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='640' height='480'><param name='movie' value='LifeSaving_converted.swf' /><param name='wmode' value='transparent'><!--[if !IE]>--><object type='application/x-shockwave-flash' data='LifeSaving_converted.swf' width='640' height='480'><!--<![endif]--><p>Flash Not Installed</p><!--[if !IE]>--></object><!--<![endif]--></object>";
 
 
 $.fx.interval = 1;
