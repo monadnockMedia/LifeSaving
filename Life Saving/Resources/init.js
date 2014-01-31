c4daae4293c9b20f3da2a6702d073e18987364ba
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2014-01-31 10:47:55 -0500

    Adding nodewebkit version

--- Life Saving/Resources/init.js
+++ Life Saving/Resources/init.js
@@ -6,11 +6,11 @@ ajSettings.url = "http://monadnock.or.gs:8080/lifeSaving/LAKE/Superior";
 var ajResp;
 var timer;
 
-var clickSnd = Ti.Media.createSound('SOUND/click.wav');
+var clickSnd = new Audio("SOUND/click.wav");
 
 var videoHTML;
 
-videoHTML = "<object id='myId' classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='640' height='480'><param name='movie' value='LifeSaving_converted.swf' /><param name='wmode' value='transparent'><!--[if !IE]>--><object type='application/x-shockwave-flash' data='LifeSaving_converted.swf' width='640' height='480'><!--<![endif]--><p>Flash Not Installed</p><!--[if !IE]>--></object><!--<![endif]--></object>";
+videoHTML = "<object id='myId' classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='640' height='480'><param name='movie' value='LifeSaving_converted.swf' /><param name='wmode' value='window'><param name='bgcolor' value='#000000'><!--[if !IE]>--><object type='application/x-shockwave-flash' data='LifeSaving_converted.swf' width='640' height='480'><param name='movie' value='LifeSaving_converted.swf' /><param name='wmode' value='window'><param name='bgcolor' value='#000000'><!--<![endif]--><p>Flash Not Installed</p><!--[if !IE]>--></object><!--<![endif]--></object>";
 
 
 $.fx.interval = 1;
